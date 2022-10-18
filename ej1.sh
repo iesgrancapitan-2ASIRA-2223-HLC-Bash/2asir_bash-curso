356cd7b1ab8434d5ec03a23263cb3a60f38de154
emilio <a19caguem@iesgrancapitan.org>
2022-10-18 09:58:32 +0000

    Primer commit

---
+++ ej1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script: ej1.sh
+# Autor: Emilio Campos
+# Descripcion: 
+
+
+#Limpiamos la pantalla
+clear
+
+#Mostramos el usuario
+echo "El usuario que ejecuta el script es:" $usuario
+
+#Mostramos el directorio personal
+echo "El directorio personal es: /home/$usuario"
+
+#Mostramos la hora
+echo "La del sistema es: $fecha"
+
+
+
+
+
---
+++ ej4.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: ej4
+# Autor: Emilio Campos
+# Descripcion: 
+
+dia=`date +%d`
+mes=`date +%b` 
+anno=`date +%Y`
+
+echo "Hoy es dia $dia del mes $mes del año $anno"
