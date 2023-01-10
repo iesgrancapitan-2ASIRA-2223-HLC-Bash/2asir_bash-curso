6aeab4c3639a2cc3214e63b3ad6f6dc88029ae89
losami <a21losami@iesgrancapitan.org>
2023-01-10 11:07:41 +0000

    Creacion de carpeta de ejercicios miguel_lopez

---
+++ ejerciciosbash/miguel_lopez/ejercicio16/ejercicio16.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Script:	ejercicio16.sh
+# Autor:	Miguel Ángel López Sánchez
+# Fecha:	10/01/2023
+# Descripción: Escribir un script que al ejecutarlo devuelva una lista con los tipos de archivo que
+# tiene un directorio pasado como parámetro. Pista: : bucle for.
