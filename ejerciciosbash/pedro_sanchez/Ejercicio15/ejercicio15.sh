aca47c7977b32430129f2f6742303255c4176dde
pedro <a21samope@iesgrancapitan.org>
2023-01-10 11:13:07 +0000

    Ejercicio15

---
+++ ejerciciosbash/pedro_sanchez/Ejercicio15/ejercicio15.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#script:       	Ejercicio15.sh
+# autor:        pedro
+# fecha:	10/01/2023
+# Descripcion:	Escribir un script que visualice por pantalla cuanto ocupan los archivos con una
+#		determinada extensión que hay en un directorio, ambos pasados como parámetros.
+#		Si no se pasa el directorio como parámetro, entonces será por defecto el directorio
+#		actual. Pista: bucle for, comando stat y expresión aritmética
