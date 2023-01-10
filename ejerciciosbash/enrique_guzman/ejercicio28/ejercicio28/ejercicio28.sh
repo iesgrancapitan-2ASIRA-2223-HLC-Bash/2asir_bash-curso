b83eedd2711e5e79bb9b1b1a2fd4c1c46b5923a1
Kikee11 <a21gumeen@iesgrancapitan.org>
2023-01-10 11:05:57 +0000

    Carpeta Enrique

---
+++ ejerciciosbash/enrique_guzman/ejercicio28/ejercicio28/ejercicio28.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Descripcion: Escribir un script que comprueba si existe la carpeta /tmp/uid, siendo uid el
+#identificador del usuario que ejecuta. Si no existe, se crea y se anula la salida de
+#error. Posteriormente hay que comprobar que la creación del directorio ha tenido
+#éxito. Pista: expansión de comando con id.
+
+#Autor: Enrique Guzmán Mesa
+
+
+
