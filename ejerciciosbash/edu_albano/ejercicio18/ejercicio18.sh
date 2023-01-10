a03e478e4a361674111bae10e967fe8a7fb3a282
SandraGin <a19jicasa@iesgrancapitan.org>
2023-01-10 11:43:06 +0000

    moviendo edu

---
+++ ejerciciosbash/edu_albano/ejercicio18/ejercicio18.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Escribir un script que recibe un parámetro que indica que información hay que
+#visualizar por pantalla. Esta puede ser: Nombre del host (hostname), nombre del
+#kernel, versión del kernel, sistema operativo y plataforma hardware (32 o 64 bits).
+#Pista: estructura case y comando uname.
+
+#Autor: Eduardo Albano Carmona
