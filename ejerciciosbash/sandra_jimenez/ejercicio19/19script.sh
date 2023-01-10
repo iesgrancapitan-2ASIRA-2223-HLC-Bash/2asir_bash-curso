67db4e90904e2d059b4be93a621781d43253d8c1
SandraGin <a19jicasa@iesgrancapitan.org>
2023-01-10 11:03:08 +0000

    carpetaSandra

---
+++ ejerciciosbash/sandra_jimenez/ejercicio19/19script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script:	19script.sh
+# Autor:	Sandra
+# Fecha:	10/01/2023
+# Descripción: 19. Escribir un script que recibe como parámetro un nombre de usuario y visualiza en
+# pantalla: Nombre completo, identificador de usuario y directorio personal. Pista:
+# Utilizar expansión de comando y tuberías con grep y cut.
+
